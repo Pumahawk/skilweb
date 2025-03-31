309c0dc31094eadd482886bdd12710127054b893
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-03-31 08:19:34 +0200

    Add database connection

--- main/main.go
+++ main/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -8,6 +10,9 @@ import (
 
 	"github.com/pumahawk/skilweb/controllers"
 	"github.com/pumahawk/skilweb/server"
+	"github.com/pumahawk/skilweb/services"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -16,7 +21,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("main: Invalid flags. %v", err)
 	}
-	err = StartHttpServer(conf)
+	db, err := GetDB(conf)
+	if err != nil {
+		log.Fatalf("main: Unable get DB object. %v", err)
+	}
+	err = StartHttpServer(conf, db)
 	if err != nil {
 		log.Fatalf("main: Unable start HttpServer. %v", err)
 	}
@@ -27,12 +36,13 @@ type HttpChain = func(http.HandlerFunc) http.HandlerFunc
 func LoadFlags() (*Conf, error) {
 	var conf Conf
 	flag.StringVar(&conf.Address, "address", ":8000", "Http server address")
+	flag.StringVar(&conf.DBConn, "db", "", "Database access string")
 	flag.Parse()
 	return &conf, nil
 }
 
-func StartHttpServer(conf *Conf) error {
-	err := LoadServerControllers()
+func StartHttpServer(conf *Conf, db *sql.DB) error {
+	err := LoadServerControllers(db)
 	if err != nil {
 		return fmt.Errorf("main server: Unable to load controllers. %w", err)
 	}
@@ -48,15 +58,16 @@ func StartHttpServer(conf *Conf) error {
 
 type Conf struct {
 	Address string
+	DBConn  string
 }
 
-func LoadServerControllers() error {
-	http.HandleFunc("/hello", BaseChain(server.ControllerViewHandler(controllers.HelloWorld)))
-	http.HandleFunc("POST /projects", BaseChain(server.ControllerViewHandler(controllers.ProjectCreate)))
-	http.HandleFunc("GET /projects", BaseChain(server.ControllerViewHandler(controllers.ProjectCreateForm)))
-	http.HandleFunc("GET /projects/search", BaseChain(server.ControllerViewHandler(controllers.ProjectSearch)))
-	http.HandleFunc("GET /projects/{id}", BaseChain(server.ControllerViewHandler(controllers.ProjectDetails)))
-	http.HandleFunc("/", BaseChain(server.ControllerViewHandler(controllers.NotFound)))
+func LoadServerControllers(db *sql.DB) error {
+	http.HandleFunc("/hello", BaseChain(db, server.ControllerViewHandler(controllers.HelloWorld)))
+	http.HandleFunc("POST /projects", BaseChain(db, server.ControllerViewHandler(controllers.ProjectCreate)))
+	http.HandleFunc("GET /projects", BaseChain(db, server.ControllerViewHandler(controllers.ProjectCreateForm)))
+	http.HandleFunc("GET /projects/search", BaseChain(db, server.ControllerViewHandler(controllers.ProjectSearch)))
+	http.HandleFunc("GET /projects/{id}", BaseChain(db, server.ControllerViewHandler(controllers.ProjectDetails)))
+	http.HandleFunc("/", BaseChain(db, server.ControllerViewHandler(controllers.NotFound)))
 	return nil
 }
 
@@ -90,6 +101,32 @@ func Chain(handler http.HandlerFunc, chain ...HttpChain) http.HandlerFunc {
 	}
 }
 
-func BaseChain(handler http.HandlerFunc) http.HandlerFunc {
-	return Chain(handler, LogHandler)
+func BaseChain(db *sql.DB, handler http.HandlerFunc) http.HandlerFunc {
+	return Chain(handler, LogHandler, DatabaseChain(db))
+}
+
+func DatabaseChain(db *sql.DB) HttpChain {
+	return func(handler http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			conn, err := db.Conn(r.Context())
+			if err != nil {
+				log.Printf("main db handler: Unable to open request db connection. %v", err)
+			}
+			rctx := context.WithValue(r.Context(), services.DBConnK, conn)
+			rc := r.WithContext(rctx)
+			handler(w, rc)
+		}
+	}
+}
+
+func GetDB(conf *Conf) (*sql.DB, error) {
+	db, err := sql.Open("postgres", conf.DBConn)
+	if err != nil {
+		return nil, fmt.Errorf("main: Unable to init db object. %w", err)
+	}
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("main: Unable to ping database. %w", err)
+	}
+
+	return db, nil
 }
